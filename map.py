e44a265bb18272e7245f72b99882d6b573d2531a
Madhuran Thiagarajah <madhuran.v@gmail.com>
2019-03-30 15:07:39 -0400

    Updated map.py

--- map.py
+++ map.py
@@ -1,7 +1,25 @@
 import folium
+import pandas as pd
+
+
+
+
+from folium.plugins import HeatMap
+
+
+def generateBaseMap(default_location=[43.7001114, -79.416297], default_zoom_start=12):
+    base_map = folium.Map(location=default_location, control_scale=True, zoom_start=default_zoom_start)
+    return base_map
+
+
+df_locations = pd.read_csv('traffic_signals.csv', header=1 )
+df_locations['count']=1
 
 # Create map object
-m=folium.Map(location=[43.7001114, -79.416297], zoom_start=12)
+m = generateBaseMap()
+
+HeatMap(data=df_locations[['Latitude', 'Longitude', 'count']].groupby(['Latitude', 'Longitude']).sum().reset_index().values.tolist(), radius=8, max_zoom=13).add_to(m)
+
 
 #Global tooltip
 tooltip = 'Discarded Needles'
@@ -33,4 +51,4 @@ folium.CircleMarker(
 
 
 #generate map
-m.save('map.html')
+m.save('index.html')
